ba1973692804400a5c8c7d94f79312c2500ac7a1
Gokulakrishnan <gokulakrishnanvgl05@gmail.com>
2024-03-23 02:15:35 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import react from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signout from './pages/Signout'; // Corrected component name
 
-export default function App(){
-  return ( 
-    <div> <h1 className='text-red'> App</h1> </div>
-  )
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-out" element={<Signout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        {/* Add a catch-all route for unmatched paths */}
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
+ 
