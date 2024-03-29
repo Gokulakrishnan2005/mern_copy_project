86e2ac20b14c28fb91b0ed9853972ad672e5cdbb
Gokulakrishnan <gokulakrishnanvgl05@gmail.com>
2024-03-29 21:11:39 +0530

    commited the backend server

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths to your template files
+  ],
+  theme: {
+    extend: {}, // Add your custom theme configurations here
+  },
+  plugins: [], // Add any additional Tailwind plugins you want to use
+};
