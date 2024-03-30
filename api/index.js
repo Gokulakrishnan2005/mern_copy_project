cf0d055597033b0f8c53c302a1185550bd58a682
Gokulakrishnan <gokulakrishnanvgl05@gmail.com>
2024-03-30 17:01:34 +0530

    created and connected to the middle ware

--- api/index.js
+++ api/index.js
@@ -1,8 +1,38 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+
+dotenv.config();
+
+
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+  console.log('connected to Mongoose!');
+  }).catch((err)=>{
+    console.log(err);
+  });
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, ()=>{
   console.log('listening on port 3000');
 });
 
+
+app.use("/api/user",userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next)=> {
+  const statusCode =err.statusCode || 500;
+  const message = err.message || 'internal server error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
